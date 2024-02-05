6ce20317714a55f125b9e24f110f0a082f5f3f20
John David Dela Cruz <johndaviddc1@gmail.com>
2024-02-05 15:07:18 +0800

    document management api

---
+++ src/test/java/dave/dev/documentmanagement/DocumentmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.documentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
