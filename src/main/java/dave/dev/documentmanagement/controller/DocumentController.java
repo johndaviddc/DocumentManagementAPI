3949e5835f5c4bf42468172aaa8acf21c344ea9d
John David Dela Cruz <johndaviddc1@gmail.com>
2024-02-13 14:50:41 +0800

    added controller package

---
+++ src/main/java/dave/dev/documentmanagement/controller/DocumentController.java
@@ -0,0 +1,23 @@
+package dave.dev.documentmanagement.controller;
+
+import dave.dev.documentmanagement.model.Document;
+import dave.dev.documentmanagement.service.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/documents")
+public class DocumentController {
+
+    @Autowired
+    private DocumentService documentService;
+
+    @GetMapping
+    public List<Document> getAllDocuments() {
+        return documentService.getAllDocuments();
+    }
+}
